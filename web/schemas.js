fb0c20a382c1a79b01b7126a73f0d7f0a9c017fa
xingxianli <xingxian@umich.edu>
2025-03-12 13:55:41 -0400

    Move dist files to root

--- web/schemas.js
+++
@@ -1,9 +0,0 @@
-import menu_ from './templates/menu.json';
-
-export const menu = menu_;
-export const getSchema = async (schema_folder) => {
-  return (await import(`./templates/${schema_folder}/schema.json`)).default;
-};
-export const getExportFormats = async (schema_folder) => {
-  return (await import(`./templates/${schema_folder}/export.js`)).default;
-};
